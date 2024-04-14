63298e353d8d5ce4b48bbae68ce3cceeb3d55e89
hminh2027 <dackheros@gmail.com>
2024-04-14 22:37:52 +0700

    done

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://ae0c61849655c46e581aa91d3c03386d-513419455.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
